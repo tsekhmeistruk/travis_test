71a24b357e3b818e447568ce9a4d2f93a551006b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-02-07 21:58:48 +0000

    Deploy tsekhmeistruk/travis_test to github.com/tsekhmeistruk/travis_test.git:gh-pages

---
+++ main_app.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+
+
+def run_script():
+    driver = webdriver.PhantomJS()
+    base_url = "https://moscowshow.com/"
+    article_xpath = "//*[@id='main']//article"
+    driver.get(base_url)
+    articles = driver.find_elements_by_xpath(article_xpath)
+    number_of_articles = str(len(articles))
+
+    with open('index.html', 'w') as file:
+            file.write(number_of_articles)
+
+    driver.close()
+
+
+if __name__ == "__main__":
+    run_script()
